e921c7eea192835c80cfeaa49a1b5d6daf45ee81
Dushani-Ranasinghe <uththaradushani@gmail.com>
2024-08-02 01:40:19 +0530

    handle states in sign in form

--- client/src/components/UI/SignInForm.js
+++ client/src/components/UI/SignInForm.js
@@ -1,18 +1,44 @@
-import React from "react";
-import "../../styles/SignInForm.css"
+import React, { useState } from "react";
+import "../../styles/SignInForm.css";
+import axios from "axios";
 
 const SignInForm = () => {
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+  });
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios
+      .post("http://localhost:3000/auth/adminlogin")
+      .then((result) => console.log(result))
+      .catch((err) => console.log(err));
+  };
   return (
-    <form className="sign-in-form">
+    <form className="sign-in-form" onSubmit={handleSubmit}>
       <div className="form-content">
-        <label htmlFor="email">Email:</label>
-        <input type="email" id="email" name="email" placeholder="Enter Email" required />
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Enter Email"
+          required
+          onChange={(e) => setValues({ ...values, email: e.target.value })}
+        />
       </div>
       <div className="form-content">
-        <label htmlFor="password">Password:</label>
-        <input type="password" id="password" name="password" placeholder="Enter Password" required/>
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          placeholder="Enter Password"
+          required
+          onChange={(e) => setValues({ ...values, password: e.target.value })}
+        />
       </div>
-      <button type="submit" >Log In</button>
+      <button type="submit">Log In</button>
     </form>
   );
 };
