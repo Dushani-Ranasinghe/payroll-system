f7ab23c84f387eeabc2db7da2bef844bff3b6577
Dushani-Ranasinghe <uththaradushani@gmail.com>
2024-08-01 20:47:14 +0530

    Removed react logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
        <h1> Payroll app</h1> 
         
       </header>
