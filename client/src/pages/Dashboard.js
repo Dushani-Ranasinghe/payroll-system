b356aab4f01dbdd75c9190cec74ca995b8231c06
Dushani-Ranasinghe <uththaradushani@gmail.com>
2024-08-02 16:17:16 +0530

    Created sidebar for dashboard

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,9 +1,58 @@
-import React from 'react'
+import React from 'react';
+import { Link, Outlet } from 'react-router-dom';
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "../styles/Dashboard.css";
 
-const Dashboard = () => {
+const Dashboard = ({ handleLogout }) => {
   return (
-    <div><h1>Dashboard</h1></div>
-  )
-}
+    <div className="dashboard-container">
+      <div className="dashboard-row">
+        <div className="dashboard-sidebar">
+          <a href="/dashboard" className="dashboard-header">
+            DPL Bags
+          </a>
+          <ul className="dashboard-nav">
+            <li className="dashboard-nav-item">
+              <Link to="/dashboard" className="dashboard-nav-link">
+                <i className="dashboard-nav-icon bi-house"></i>
+                <span className="dashboard-nav-text">Dashboard</span>
+              </Link>
+            </li>
+            <li className="dashboard-nav-item">
+              <Link to="/employee" className="dashboard-nav-link">
+                <i className="dashboard-nav-icon bi-people"></i>
+                <span className="dashboard-nav-text">Employees</span>
+              </Link>
+            </li>
+            <li className="dashboard-nav-item">
+              <Link to="/employee" className="dashboard-nav-link">
+                <i className="dashboard-nav-icon bi-cash-coin"></i>
+                <span className="dashboard-nav-text">Payroll</span>
+              </Link>
+            </li>
+            <li className="dashboard-nav-item">
+              <Link to="profile" className="dashboard-nav-link">
+                <i className="dashboard-nav-icon bi-person-circle"></i>
+                <span className="dashboard-nav-text">Profile</span>
+              </Link>
+            </li>
+            <li className="dashboard-nav-item" onClick={handleLogout}>
+              <a href="/logout" className="dashboard-nav-link">
+                <i className="dashboard-nav-icon bi-power"></i>
+                <span className="dashboard-nav-text">Logout</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="dashboard-content">
+          <div className="dashboard-header-bar">
+            <h4 className="dashboard-title">Employee Management System</h4>
+          </div>
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
